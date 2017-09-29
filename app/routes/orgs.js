672e4ec19cbfb96e9c98f29518b174ad054cd3fc
Lily Beauvilliers <lily.beauvilliers@gmail.com>
2017-09-29 15:21:51 +0100

    refactor data from template to route

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {id: "Facebook"},
+      {id: "Netflix"},
+      {id: "Intercom"},
+      {id: "Ember.js"},
+    ];
+  }
 });
